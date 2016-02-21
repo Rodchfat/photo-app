67a1b934c87a356aecbd338d496a83320e2f65b5
Rails Projects <rod-rails@gmail.com>
2016-02-21 01:23:58 +0000

    another try for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { :host => 'http://railsapps-raadchfat.c9users.io:8080'}
+  #config.action_mailer.default_url_options = { :host => 'http://railsapps-raadchfat.c9users.io:8080'}
+  config.action_mailer.default_url_options = { host: '$IP', port: $PORT }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
